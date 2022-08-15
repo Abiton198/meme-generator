1ccd204c1581f9c33c6a76cc70fc7435053cb112
abiton198 <abitonp@gmail.com>
2022-08-15 09:58:41 +0200

    .

---
+++ meme-generator/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Header';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+            <Header />
+      </div>
+      )
+  }
+}
+
+export default App;
